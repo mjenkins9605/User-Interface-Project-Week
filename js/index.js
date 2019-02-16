31ca0642db42abf63da7f5277915ceda21924a9f
Michael Jenkins <mjenkins9605@vidangel.com>
2019-02-16 14:49:09 -0500

    services page laid out. fine tuning remaining

--- js/index.js
+++ js/index.js
@@ -1,12 +1,12 @@
 // JS goes here
 
-/* When the user clicks on the button, 
-toggle between hiding and showing the dropdown content */
+
+//NAV BAR
+
 function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
   }
   
-  // Close the dropdown menu if the user clicks outside of it
   window.onclick = function(event) {
     if (!event.target.matches('.hamburgerClose')) {
       let dropdowns = document.getElementsByClassName("dropdown-content");
@@ -19,3 +19,46 @@ function myFunction() {
       }
     }
   }
+
+
+
+  //TABS
+
+
+class TabLink {
+    constructor(element) {
+
+      this.element = element;
+      this.data = this.element.dataset.tab;
+      this.item = document.querySelector(`.tabs-item[data-tab="${this.data}"]`);
+      this.tabItem = new TabItem(this.item); 
+      this.element.addEventListener('click', () => {this.select()})
+    };
+  
+    select() {
+      const links = document.querySelectorAll('.tabs-link'); 
+      links.forEach( link => {
+        link.classList.remove('tabs-link-selected')
+      });
+  
+      this.element.classList.add('tabs-link-selected');
+      this.tabItem.select();
+    }
+  }
+  
+  
+  class TabItem {
+    constructor(element) {
+      this.element = element;
+    }
+  
+    select() {
+      const items = document.querySelectorAll('.tabs-item');
+      items.forEach( item => {
+        item.classList.remove('tabs-item-selected');
+      })
+      this.element.classList.add('tabs-item-selected');
+    }
+  }
+
+  const links = document.querySelectorAll('.tabs-link').forEach(link => new TabLink(link));
