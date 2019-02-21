9b1275f15cd05f96e144fda48fd86393de5f6b96
Michael Jenkins <mjenkins9605@vidangel.com>
2019-02-21 10:21:12 -0500

    MVP

--- js/index.js
+++ js/index.js
@@ -3,22 +3,6 @@
 
 //NAV BAR
 
-// function myFunction() {
-//     document.getElementById("myDropdown").classList.toggle("show");
-//   }
-  
-//   window.onclick = function(event) {
-//     if (!event.target.matches('.hamburgerClose')) {
-//       let dropdowns = document.getElementsByClassName("dropdown-content");
-//       let i;
-//       for (i = 0; i < dropdowns.length; i++) {
-//         let openDropdown = dropdowns[i];
-//         if (openDropdown.classList.contains('show')) {
-//           openDropdown.classList.remove('show');
-//         }
-//       }
-//     }
-//   }
 function openNav() {
     document.getElementById("myNav").style.height = "100%";
   }
